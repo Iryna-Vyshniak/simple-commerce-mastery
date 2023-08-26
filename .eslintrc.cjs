e52a5de00850fcad31532818aadf6d9081ae7262
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-26 12:19:02 +0300

    complete eslintrc

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react/prop-types': 0,
+    indent: ['warn', 2],
+    'import/no-absolute-path': 'off',
+    quotes: ['error', 'single'],
+    'jsx-quotes': ['error', 'prefer-double'],
+    'no-console': 'warn',
+    semi: ['error', 'always'],
+    'space-before-function-paren': [
+      'error',
+      {
+        anonymous: 'always',
+        named: 'never',
+        asyncArrow: 'always'
+      }
+    ],
+    'no-unused-vars': 'warn',
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'object', 'type'],
+        'newlines-between': 'always-and-inside-groups'
+      }
+    ],
+    'multiline-ternary': 'off'
+  }
+};
