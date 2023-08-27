9979a47840a0d9f50800cd24b0393f19712d555a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-27 19:47:45 +0300

    create constants for links and social media

---
+++ src/constants/index.js
@@ -0,0 +1,14 @@
+import { facebook, instagram, twitter } from '../assets/icons';
+
+export const navLinks = [
+  { href: '#home', label: 'Home' },
+  { href: '#about-us', label: 'About Us' },
+  { href: '#products', label: 'Products' },
+  { href: '#contact-us', label: 'Contact Us' }
+];
+
+export const socialMedia = [
+  { src: facebook, alt: 'facebook logo' },
+  { src: twitter, alt: 'twitter logo' },
+  { src: instagram, alt: 'instagram logo' }
+];
