8aac82259f5a1b5f0b2ecdec5dd1516782790db8
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-31 01:17:56 +0300

    add array of shoes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,12 @@
 import { facebook, instagram, twitter } from '../assets/icons';
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3
+} from '../assets/images';
 
 export const navLinks = [
   { href: '#home', label: 'Home' },
@@ -48,4 +56,19 @@ export const statistics = [
   { value: '1k+', label: 'Brands' },
   { value: '500+', label: 'Shops' },
   { value: '250k+', label: 'Customers' }
+];
+
+export const shoes = [
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    bigShoe: bigShoe2
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3
+  }
 ];
