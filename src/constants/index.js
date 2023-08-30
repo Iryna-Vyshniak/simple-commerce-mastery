fb324aa1a39eb4eff2cb09e35127257b4e71046b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-30 23:43:16 +0300

    add statistics

--- src/constants/index.js
+++ src/constants/index.js
@@ -43,3 +43,9 @@ export const footerLinks = [
     ]
   }
 ];
+
+export const statistics = [
+  { value: '1k+', label: 'Brands' },
+  { value: '500+', label: 'Shops' },
+  { value: '250k+', label: 'Customers' }
+];
