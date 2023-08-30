60fafa02bba29f61583c58d1f3e24dd3b4244b39
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-31 01:19:17 +0300

    complete header with navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,11 @@
+import Nav from './Nav';
+
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="absolute top-0 left-0 z-40 padding-x py-8 w-full">
+      <Nav />
+    </header>
+  );
 };
 
 export default Header;
