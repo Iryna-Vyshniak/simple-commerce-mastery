17b6e0db06ccb69844f631470b5db7c3e8386ccf
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-31 01:17:30 +0300

    add styles

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,5 +1,9 @@
 const MainTitle = ({ children }) => {
-  return <h1>{children}</h1>;
+  return (
+    <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+      {children}
+    </h1>
+  );
 };
 
 export default MainTitle;
