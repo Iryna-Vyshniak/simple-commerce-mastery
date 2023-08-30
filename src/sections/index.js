dd01dc4044e4c4dc317642b0e78b38d69e3bae4b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-30 19:01:00 +0300

    add reexport sections component

---
+++ src/sections/index.js
@@ -0,0 +1,9 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import SpecialOffers from './SpecialOffers';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+
+export { Hero, PopularProducts, SuperQuality, Services, SpecialOffers, CustomerReviews, Subscribe };
