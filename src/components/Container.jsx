907a63e2adadb303943692cb06f3b0ae7be2a402
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-08-31 01:17:16 +0300

    add dynamic styles

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,5 +1,6 @@
-const Container = ({ children }) => {
-  return <div className="container mx-auto my-0">{children}</div>;
+const Container = ({ children, className }) => {
+  const containerClasses = `max-container ${className || ''}`;
+  return <div className={containerClasses}>{children}</div>;
 };
 
 export default Container;
