576ed22b5d1815539f741e3f42982642dfcb5739
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-02 16:02:39 +0300

    complete customer reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,21 @@
 import Container from '../components/Container';
+import ReviewList from '../components/Review/ReviewList';
 
 const CustomerReviews = () => {
-  return <Container>CustomerReviews</Container>;
+  return (
+    <Container>
+      <h3 className="font-palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customers </span>
+        Say?
+      </h3>
+      <p className="m-auto mt-4 max-w-lg  text-center info-text">
+        Hear genuine stories from our satisfied customers about their exceptional experiences with
+        us.
+      </p>
+      <ReviewList />
+    </Container>
+  );
 };
 
 export default CustomerReviews;
