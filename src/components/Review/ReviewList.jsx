d50b553f345cbea2e3eb6c8e518f745559bb4f1f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-02 16:02:04 +0300

    create and complete review list

---
+++ src/components/Review/ReviewList.jsx
@@ -0,0 +1,21 @@
+import { reviews } from '../../constants';
+
+import ReviewCard from './ReviewCard';
+
+const ReviewList = () => {
+  return (
+    <ul className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+      {reviews.map(({ avatarURL, name, rating, feedback }, idx) => (
+        <ReviewCard
+          key={idx}
+          avatarURL={avatarURL}
+          name={name}
+          rating={rating}
+          feedback={feedback}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ReviewList;
