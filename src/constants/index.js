dd84a8f0131faf0718e895bb925c9edd50896cca
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-02 15:41:48 +0300

    add array of reviews

--- src/constants/index.js
+++ src/constants/index.js
@@ -72,3 +72,22 @@ export const shoes = [
     bigShoe: bigShoe3
   }
 ];
+
+export const reviews = [
+  {
+    avatarURL:
+      'https://res.cloudinary.com/dkqxaid79/image/upload/v1692265419/rewievs/Fletcher_lyecuy.png',
+    name: 'Morich Brown',
+    rating: 4.5,
+    feedback:
+      'The attention to detail and the quality of the product exceeded my expectations. Highly recommended!'
+  },
+  {
+    avatarURL:
+      'https://res.cloudinary.com/dkqxaid79/image/upload/v1691497617/rewievs/image_70_j8immx.png',
+    name: 'Lota Mongeskar',
+    rating: 5,
+    feedback:
+      'The product not only met but exceeded my expectations. I`ll definitely be a returning customer!'
+  }
+];
