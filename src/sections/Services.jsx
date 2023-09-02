efa92e397bc2486215de19a2ce32bca38637220e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-02 16:29:14 +0300

    complete services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,7 +1,15 @@
 import Container from '../components/Container';
+import ServiceCard from '../components/ServiceCard';
+import { services } from '../constants';
 
 const Services = () => {
-  return <Container>Services</Container>;
+  return (
+    <Container className="flex justify-center flex-wrap gap-9">
+      {services.map(service => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </Container>
+  );
 };
 
 export default Services;
