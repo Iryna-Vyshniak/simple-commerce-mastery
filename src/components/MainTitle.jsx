94ba42699d5c1a73a2bcd4c397f49df91629c9e7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 16:11:04 +0300

    update fonts

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,6 +1,6 @@
 const MainTitle = ({ children }) => {
   return (
-    <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+    <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[52px] max-sm:leading-[62px] font-bold">
       {children}
     </h1>
   );
