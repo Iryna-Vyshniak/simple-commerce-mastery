8e1c3e39b3469d0e156e34a82ae69a284b55f66f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 21:36:26 +0300

    create and complete button group

---
+++ src/components/ButtonGroup.jsx
@@ -0,0 +1,35 @@
+import Button from './Button';
+
+const ButtonGroup = () => {
+  return (
+    <div className="flex space-x-4 mb-6 text-sm font-medium">
+      <div className="flex-auto flex space-x-4">
+        <Button type="submit" label="Buy now" className="w-[140px] h-9 text-sm" />
+        <Button
+          backgroundColor="bg-transparent"
+          textColor="text-slate-900"
+          borderColor="border-slate-200"
+          className="w-[150px] h-10 px-6 text-sm rounded-md"
+          type="button"
+          label="Add to bag"
+        />
+      </div>
+
+      <button
+        className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
+        type="button"
+        aria-label="Like"
+      >
+        <svg width="20" height="20" fill="currentColor" aria-hidden="true">
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default ButtonGroup;
