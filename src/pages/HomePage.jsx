08dae95f7cff4a85da12222f3f2ab5204df2ab58
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 09:58:43 +0300

    refactor, used mapping to create partitions based on data

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,37 +1,13 @@
-import {
-  Hero,
-  PopularProducts,
-  SuperQuality,
-  Services,
-  SpecialOffers,
-  CustomerReviews,
-  Subscribe
-} from '../sections';
+import { sectionsData } from '../constants';
 
 const HomePage = () => {
   return (
     <>
-      <section id="home" className="xl:padding-1 wide:padding-r padding-b">
-        <Hero />
-      </section>
-      <section id="products" className="padding">
-        <PopularProducts />
-      </section>
-      <section id="about-us" className="padding">
-        <SuperQuality />
-      </section>
-      <section className="padding-x py-10">
-        <Services />
-      </section>
-      <section className="padding">
-        <SpecialOffers />
-      </section>
-      <section className="padding bg-primary">
-        <CustomerReviews />
-      </section>
-      <section id="contact-us" className="padding-x sm:py-32 py-16 w-full">
-        <Subscribe />
-      </section>
+      {sectionsData.map(({ id, className, component }, idx) => (
+        <section key={idx} id={id} className={className}>
+          {component}
+        </section>
+      ))}
     </>
   );
 };
