8b0b8834f1812a7368a9e5573d6a234b2de8f8e4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 20:55:15 +0300

    add geometry

--- src/components/MainTitle.jsx
+++ src/components/MainTitle.jsx
@@ -1,6 +1,6 @@
 const MainTitle = ({ children }) => {
   return (
-    <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[52px] max-sm:leading-[62px] font-bold">
+    <h1 className="mt-10 mb-10 font-palanquin text-8xl max-sm:text-[52px] max-sm:leading-[62px] font-bold">
       {children}
     </h1>
   );
