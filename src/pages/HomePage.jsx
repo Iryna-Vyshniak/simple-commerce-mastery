927f4db52f986f76b3db776f41dca361ed1a738b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 16:15:03 +0300

    add scroll  component that listens for location changes and determines if there is a hash (or element ID) that  app should scroll

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
+import ScrollToAnchor from '../components/ScrollToAnchor';
 import { sectionsData } from '../constants';
 
 const HomePage = () => {
   return (
     <>
+      <ScrollToAnchor />
       {sectionsData.map(({ id, className, component }, idx) => (
         <section key={idx} id={id} className={className}>
           {component}
