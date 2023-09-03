a6d24b0b085f1ef1533aa35ef476d505bedc8ecb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 21:37:24 +0300

    add conditions to render styles

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,21 +15,29 @@ const Nav = () => {
   const onCloseNavbar = () => setNavbarOpen(false);
 
   return (
-    <nav className="relative flex flex-wrap justify-between items-center max-container">
+    <nav className="relative flex flex-wrap justify-between items-center max-container px-2">
       <Link to="/">
         <img
           src={headerLogo}
           alt="logo"
           width={129}
           height={29}
-          className="relative z-40 m-0 w-[129px] h-[29px]"
+          className={
+            navbarOpen
+              ? 'fixed z-40 m-0 w-[129px] h-[29px]'
+              : 'relative z-40 m-0 w-[129px] h-[29px]'
+          }
         />
       </Link>
       <NavMenu />
       <button
         type="button"
         onClick={() => setNavbarOpen(!navbarOpen)}
-        className="relative z-40 hidden max-lg:block cursor-pointer"
+        className={
+          navbarOpen
+            ? 'max-lg:fixed top-8 right-20 z-40 hidden max-lg:block cursor-pointer text-2xl'
+            : 'relative z-40 hidden max-lg:block cursor-pointer'
+        }
       >
         {navbarOpen ? 'X' : <img src={hamburger} alt="hamburger icon" width={25} height={25} />}
       </button>
