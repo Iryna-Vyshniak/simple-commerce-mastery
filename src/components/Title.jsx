b0b75fcd7ad2c59dfea01bbcf86d7569d7bbabba
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 20:54:24 +0300

    add UI

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,9 @@
 const Title = ({ children }) => {
-  return <h2>{children}</h2>;
+  return (
+    <h2 className="mt-10 font-palanquin text-3xl max-sm:text-[32px] max-sm:leading-[22px] font-bold text-left">
+      {children}
+    </h2>
+  );
 };
 
 export default Title;
