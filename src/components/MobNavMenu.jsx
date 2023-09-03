81807fe585a052058a0b181b5a1c833434706155
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-03 21:36:01 +0300

    add link to scroll component

--- src/components/MobNavMenu.jsx
+++ src/components/MobNavMenu.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import { Link } from 'react-router-dom';
+
 import { navLinks } from '../constants';
 
 const MobNavMenu = ({ onClose }) => {
@@ -12,15 +14,15 @@ const MobNavMenu = ({ onClose }) => {
 
   const item = navLinks.map(({ href, label }) => (
     <li key={label}>
-      <a
-        href={href}
-        className={`cursor-pointer text-xl text-center font-montserrat leading-normal  ${
+      <Link
+        to={`/${href}`}
+        className={`cursor-pointer text-3xl text-center font-montserrat leading-normal  ${
           activeLink === href ? 'font-bold text-deep-red' : 'text-slate-gray'
         } hover:text-deep-red focus:text-deep-red focus:outline-none transition duration-250 ease-in-out`}
         onClick={() => handleNavClick(href)}
       >
         {label}
-      </a>
+      </Link>
     </li>
   ));
   return (
