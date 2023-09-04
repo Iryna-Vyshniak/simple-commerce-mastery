251a5bcf2329d49516a4fa7f38702a1b47e9da9a
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-04 10:21:20 +0300

    create and complete thumb image component

---
+++ src/components/ThumbImage.jsx
@@ -0,0 +1,20 @@
+import Image from './Image';
+
+const ThumbImage = ({ src, alt, className }) => {
+  return (
+    <div className="relative flex-auto overflow-hidden">
+      <Image
+        src={src}
+        alt={alt}
+        className={
+          className
+            ? `block object-contain object-center ${className}`
+            : 'block max-w-full h-auto  object-contain object-center'
+        }
+        loading="lazy"
+      />
+    </div>
+  );
+};
+
+export default ThumbImage;
