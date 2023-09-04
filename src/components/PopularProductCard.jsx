a0e24f9a7632726f3a5d14554bf34fce78f3e1ea
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-04 10:22:34 +0300

    refactor, complete with components

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,22 +1,24 @@
 import { star } from '../assets/icons';
 
 import Image from './Image';
+import Price from './Price';
+import ThumbImage from './ThumbImage';
 
 const PopularProductCard = ({ imgURL, name, price }) => {
   return (
-    <li className="flex flex-1 flex-col items-center justify-center w-full max-sm:w-full">
-      <div className="flex items-center justify-center w-[282px] h-[282px]">
-        <Image src={imgURL} alt={name} className="object-contain object-center" />
-      </div>
+    <li className="flex flex-1 flex-col items-center justify-center p-3 w-full max-sm:w-full shadow-xl border-2 rounded-xl border-white cursor-pointer active:border-deep-red active:shadow-md transition duration-200 ease-in-out">
+      <ThumbImage
+        src={imgURL}
+        alt={name}
+        className="flex items-center justify-center w-[282px] h-[250px]"
+      />
 
       <div className="mt-8 flex justify-start gap-2.5">
         <Image src={star} alt="rating icon" width={24} height={24} />
         <p className="font-montserrat text-xl leading-normal text-slate-gray">(4.5)</p>
       </div>
       <h3 className="mt-2 text-2xl leading-normal font-semibold font-palanquin">{name}</h3>
-      <p className="mt-2 font-semibold font-montserrat text-deep-red text-2xl leading-normal">
-        {price}
-      </p>
+      <Price amount={price} />
     </li>
   );
 };
