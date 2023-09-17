f76c9e06ac9f254e1f9da9c5c6ce120e4aa47a16
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-17 12:25:09 +0300

    refactor with hook useToggle

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,18 +1,16 @@
 import { Link } from 'react-router-dom';
 
-import { useState } from 'react';
-
 import { headerLogo } from '../assets/images';
 
 import { hamburger } from '../assets/icons';
+
+import { useToggle } from '../shared/hooks';
 
 import NavMenu from './NavMenu';
 import MobNavMenu from './MobNavMenu';
 
 const Nav = () => {
-  const [navbarOpen, setNavbarOpen] = useState(false);
-
-  const onCloseNavbar = () => setNavbarOpen(false);
+  const { isOpen, close, toggle } = useToggle(false);
 
   return (
     <nav className="relative flex flex-wrap justify-between items-center max-container px-2">
@@ -23,33 +21,31 @@ const Nav = () => {
           width={129}
           height={29}
           className={
-            navbarOpen
-              ? 'fixed z-40 m-0 w-[129px] h-[29px]'
-              : 'relative z-40 m-0 w-[129px] h-[29px]'
+            isOpen ? 'fixed z-40 m-0 w-[129px] h-[29px]' : 'relative z-40 m-0 w-[129px] h-[29px]'
           }
         />
       </Link>
       <NavMenu />
       <button
         type="button"
-        onClick={() => setNavbarOpen(!navbarOpen)}
+        onClick={toggle}
         className={
-          navbarOpen
+          isOpen
             ? 'max-lg:fixed top-8 right-20 z-40 hidden max-lg:block cursor-pointer text-2xl'
             : 'relative z-40 hidden max-lg:block cursor-pointer'
         }
       >
-        {navbarOpen ? 'X' : <img src={hamburger} alt="hamburger icon" width={25} height={25} />}
+        {isOpen ? 'X' : <img src={hamburger} alt="hamburger icon" width={25} height={25} />}
       </button>
 
       <div
         className={
-          navbarOpen
+          isOpen
             ? 'fixed top-0 left-0 z-30 flex items-center justify-center padding bg-primary min-h-screen w-screen lg:hidden'
             : 'hidden'
         }
       >
-        <MobNavMenu onClose={onCloseNavbar} />
+        <MobNavMenu onClose={close} />
       </div>
     </nav>
   );
