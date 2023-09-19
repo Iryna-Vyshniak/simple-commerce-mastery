5d14925f5de26f0faa76d2b2644a0f58ebf1005c
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-19 21:18:12 +0300

    Created the CartEmpty component to display an empty cart message and a button to navigate back to the Nike Store

---
+++ src/components/Cart/CartEmpty.jsx
@@ -0,0 +1,24 @@
+import { emptybag } from '../../assets/images';
+import Button from '../Button';
+
+const CartEmpty = ({ onCartToggle }) => {
+  return (
+    <>
+      <div className="flex items-center justify-center flex-col h-screen px-11 text-center gap-7">
+        <img
+          src={emptybag}
+          alt="empty bag"
+          className="w-40 lg:w-36 sm:w-28 h-auto object-fill transition-all duration-300 hover:scale-110"
+        />
+        <Button
+          type="button"
+          label="&lt; Back To Nike Store"
+          className="button-theme bg-gradient-to-b from-amber-500 to-orange-500 shadow-lg shadow-orange-500 flex items-center justify-center text-slate-900 py-2 gap-3 text-sm px-5 font-semibold active:scale-110"
+          onClick={onCartToggle}
+        />
+      </div>
+    </>
+  );
+};
+
+export default CartEmpty;
