755412f57ac18e3df8c95a78c7a7a5681beceaa4
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-19 12:20:03 +0300

    Wrapped the application with Redux 'Provider' and 'PersistGate'

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+
 import App from './App.jsx';
 import './index.css';
+import { persistor, store } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
