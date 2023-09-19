aa809b33cb1894636e148c6c9cac25f6155187c2
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-19 21:25:40 +0300

    Implement cart functionality and UI

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,68 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useEffect } from 'react';
+
+import { selectCartProducts, selectCartState } from '../../redux/cart/cart-selectors';
+
+import { setClearCart, setCloseCart, setGetTotals } from '../../redux/cart/cart-slice';
+
+import CartEmpty from './CartEmpty';
+import CartItem from './CartItem';
+import CartHeader from './CartHeader';
+import CartFooter from './CartFooter';
+
+const Cart = () => {
+  const dispatch = useDispatch();
+  const isOpen = useSelector(selectCartState);
+  const cartProducts = useSelector(selectCartProducts);
+
+  const onCartToggle = () => {
+    dispatch(setCloseCart(false));
+  };
+
+  const onClearCartItems = () => {
+    dispatch(setClearCart());
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'scroll';
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    dispatch(setGetTotals());
+  }, [cartProducts, dispatch]);
+
+  return (
+    <div
+      className={`fixed top-0 left-0 right-0 bottom-0 w-full h-screen blur-cart opacity-100 z-[50] duration-500 ${
+        isOpen ? 'opacity-100 visible translate-x-0' : 'opacity-0 invisible translate-x-8'
+      }`}
+    >
+      <div
+        className={`absolute right-0 z-[150] h-screen max-w-xl w-full blur-cart duration-500  ${
+          isOpen ? 'opacity-100 visible translate-x-0' : 'opacity-0 invisible translate-x-8'
+        }`}
+      >
+        <CartHeader onCartToggle={onCartToggle} onClearCartItems={onClearCartItems} />
+        {cartProducts?.length === 0 ? (
+          <CartEmpty onCartToggle={onCartToggle} />
+        ) : (
+          <div className="overflow-hidden overflow-y-auto overflow-x-hidden h-[76vh] scroll-style py-7">
+            <div className="flex items-start justify-start flex-col gap-y-7 h-[81vh] py-3">
+              {cartProducts?.map(item => (
+                <CartItem key={item._id} item={item} />
+              ))}
+            </div>
+          </div>
+        )}
+        <CartFooter />
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,11 +5,13 @@ import { Outlet } from 'react-router-dom';
 import Container from './Container';
 import Header from './Header';
 import Footer from './Footer/Footer';
+import Cart from './Cart/Cart';
 
 const Layout = () => {
   return (
-    <div className="relative flex flex-col items-center justify-center  min-h-screen">
+    <div className="flex flex-col items-center justify-center  min-h-screen">
       <Header />
+      <Cart />
       <main className="grow w-full mt-10">
         <Container>
           <Suspense fallback={<h3>Loading...</h3>}>
