a0913a30c09986f49fecf3ff46504a554215c5e7
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-20 22:16:40 +0300

    update styles

--- src/components/Product/ShoeCard.jsx
+++ src/components/Product/ShoeCard.jsx
@@ -14,13 +14,13 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
       } cursor-pointer max-sm:flex-1 shadow-xl transition duration-200 ease-in-out`}
       onClick={handleClick}
     >
-      <div className="flex justify-center items-center bg-primary bg-gradient-to-b from-white to-white/5  dark:shadow-slate-400 dark:bg-slate-500 sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+      <div className="flex justify-center items-center w-[128px] h-[104px] bg-primary bg-gradient-to-b from-white to-white/5  dark:shadow-slate-400 dark:bg-slate-500 sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
         <Image
           src={imgURL.thumbnail}
           alt="shoe collection"
           width={127}
           height={104}
-          className="object-contain object-center w-[127px] h-[104px]"
+          className="object-cover object-center"
         />
       </div>
     </div>
