c5c99ec6149a062aca3a0c32a0be0205a2baff0d
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-20 22:19:08 +0300

    Added conditional rendering of slides based on props

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,26 +1,22 @@
-import { useSelector } from 'react-redux';
-
-import { selectIsLoading, selectProducts } from '../../redux/products/products-selectors';
-
 import { useSlide, useVisibleSlides } from '../../shared/hooks';
 
 import PopularProductCard from '../Product/PopularProductCard';
 
 import Button from '../Button/Button';
 
-import ImageSkeleton from './ImageSceleton';
+import ShoeCard from '../Product/ShoeCard';
 
-const Slider = () => {
-  const products = useSelector(selectProducts);
-  const isLoading = useSelector(selectIsLoading);
-  const slideCount = products.length;
+const Slider = ({ products, shoes, posterShoe, setPosterShoe }) => {
+  const slideCount = products ? products?.length : shoes?.length;
+  const screenWidth = window.innerWidth;
+
   const visibleSlides = useVisibleSlides();
   const { currentIndex, prevSlide, nextSlide, goToSlide } = useSlide(0, slideCount, visibleSlides);
 
   return (
     <article className="relative flex justify-center items-center max-w-screen-[1440px]">
       <section className="flex">
-        {products.length > 0 && (
+        {(products?.length > 0 || shoes?.length > 0) && (
           <>
             <div className="flex items-center justify-center touch-auto gap-x-4 transition duration-550 ease-linear">
               <Button
@@ -29,19 +25,32 @@ const Slider = () => {
                 onClick={prevSlide}
                 className="absolute left-0 flex aspect-square w-8 -translate-x-1/2 cursor-pointer items-center justify-center self-center rounded-full bg-neutral-100 text-slate-900 transition hover:bg-slate-300 hover:text-white"
               />
-              {isLoading && <ImageSkeleton />}
-              {products
-                .slice(currentIndex, currentIndex + visibleSlides)
-                .map(({ _id, imgURL, name, price }, index) => (
-                  <PopularProductCard
-                    key={_id}
-                    _id={_id}
-                    imgURL={imgURL}
-                    name={name}
-                    price={price}
-                    onClick={() => goToSlide(index)}
-                  />
-                ))}
+              {products &&
+                products
+                  .slice(currentIndex, currentIndex + visibleSlides)
+                  .map(({ _id, imgURL, name, price }, index) => (
+                    <PopularProductCard
+                      key={_id}
+                      _id={_id}
+                      imgURL={imgURL}
+                      name={name}
+                      price={price}
+                      onClick={() => goToSlide(index)}
+                    />
+                  ))}
+
+              {shoes &&
+                shoes
+                  .slice(currentIndex, currentIndex + (screenWidth >= 960 ? 3 : 1))
+                  .map(({ _id, popular }, index) => (
+                    <ShoeCard
+                      key={_id}
+                      index={index}
+                      imgURL={popular}
+                      changeBigShoeImage={shoe => setPosterShoe(shoe)}
+                      bigShoeImg={posterShoe}
+                    />
+                  ))}
               <Button
                 type="button"
                 onClick={nextSlide}
