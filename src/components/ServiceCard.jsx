320d878cd885324598557ce80d256fe9ea179992
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-21 13:32:52 +0300

    Replaced HTML tag h3 with reusable component for enhanced reusability - Title

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,4 +1,5 @@
 import Image from './Image/Image';
+import Title from './Title/Title';
 
 const ServiceCard = ({ imgURL, label, subtext }) => {
   return (
@@ -6,9 +7,9 @@ const ServiceCard = ({ imgURL, label, subtext }) => {
       <div className="flex justify-center items-center w-11 h-11  bg-deep-red rounded-full">
         <Image src={imgURL} alt={label} width={24} height={24} />
       </div>
-      <h3 className="mt-5 font-palanquin text-3xl max-sm:text-2xl leading-normal font-bold">
+      <Title className="mt-5 font-palanquin text-3xl max-sm:text-2xl leading-normal font-bold">
         {label}
-      </h3>
+      </Title>
       <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray dark:text-slate-300">
         {subtext}
       </p>
