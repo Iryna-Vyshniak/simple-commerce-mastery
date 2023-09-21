f2971a2298f306ad66aab0c1c43a837bd9e54050
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-21 22:19:42 +0300

    Created a Category component to display a list of categories.

---
+++ src/components/Product/Category.jsx
@@ -0,0 +1,15 @@
+const Category = ({ category }) => {
+  return (
+    <ul className="flex items-center justify-center gap-4 w-full">
+      {category.map((item, idx) => (
+        <li key={idx} className="w-max py-1 px-4 shadow-lg rounded-md">
+          <p className="text-sm font-semibold dark:text-slate-400 dark:drop-shadow-[0_0.03px_0.5px_rgba(0,0,20,1)]">
+            {item}
+          </p>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Category;
