d5d385ecb36079f62ea4d124731223ce06c65f2e
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-21 17:20:54 +0300

    refactor: use dispatch for get all reviews, add slider, delete start array of reviews

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,8 +1,21 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useEffect } from 'react';
+
 import Container from '../components/Container';
-import ReviewList from '../components/Review/ReviewList';
+import Slider from '../components/Slider/Slider';
 import Title from '../components/Title/Title';
+import { selectReviews } from '../redux/reviews/reviews-selectors';
+import { getAllReviews } from '../redux/reviews/reviews-operations';
 
 const CustomerReviews = () => {
+  const dispatch = useDispatch();
+  const reviews = useSelector(selectReviews);
+
+  useEffect(() => {
+    dispatch(getAllReviews());
+  }, [dispatch]);
+
   return (
     <Container>
       <Title className="text-center">
@@ -10,11 +23,11 @@ const CustomerReviews = () => {
         <span className="text-coral-red"> Customers </span>
         Say?
       </Title>
-      <p className="m-auto mt-4 max-w-lg  text-center info-text dark:text-slate-900">
+      <p className="m-auto mb-8 mt-4 max-w-lg text-center info-text dark:text-slate-900">
         Hear genuine stories from our satisfied customers about their exceptional experiences with
         us.
       </p>
-      <ReviewList />
+      <Slider reviews={reviews} />
     </Container>
   );
 };
--- src/shared/data/index.js
+++ src/shared/data/index.js
@@ -3,19 +3,8 @@ import { navLinks } from './navLinks';
 import { socialMedia } from './socialMedia';
 import { footerLinks } from './footerLinks';
 import { statistics } from './statistics';
-import { reviews } from './reviews';
 import { services } from './services';
 import { views } from './views';
 import { offers } from './offers';
 
-export {
-  sectionsData,
-  navLinks,
-  socialMedia,
-  footerLinks,
-  statistics,
-  reviews,
-  services,
-  views,
-  offers
-};
+export { sectionsData, navLinks, socialMedia, footerLinks, statistics, services, views, offers };
