da2cf6622ac9a139c87712ae5d1e3841b2ac3869
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-23 01:38:39 +0300

    create  Counter component and add to CartItem

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,13 +1,11 @@
 import { useDispatch } from 'react-redux';
 
-import {
-  removeFromCart,
-  setDecrementProduct,
-  setIncrementProduct
-} from '../../redux/cart/cart-slice';
+import { removeFromCart } from '../../redux/cart/cart-slice';
 import Button from '../Button/Button';
 import Title from '../Title/Title';
 import ThumbImage from '../Image/ThumbImage';
+
+import Counter from './Counter';
 
 const CartItem = ({ item }) => {
   const dispatch = useDispatch();
@@ -17,12 +15,6 @@ const CartItem = ({ item }) => {
     dispatch(removeFromCart({ _id }));
   };
 
-  const handleIncrementItem = () => {
-    dispatch(setIncrementProduct({ item }));
-  };
-  const handleDecrementItem = () => {
-    dispatch(setDecrementProduct({ item }));
-  };
   const { imgURL, name, price, cartQuantity } = item;
 
   return (
@@ -42,30 +34,7 @@ const CartItem = ({ item }) => {
           <div className="grid items-center leading-none">
             <Title className="font-medium text-lg text-slate-900 lg:text-sm">{name}</Title>
           </div>
-          <div className="flex items-center justify-around w-full">
-            <Button
-              type="button"
-              svgClass="btn-cart dark:fill-slate-800"
-              svgURL="#icon-minus"
-              ariaLabel="minus product quantity"
-              onClick={handleDecrementItem}
-              backgroundColor="bg-transparent"
-              className="h-10 px-6 text-sm  flex justify-center items-center bg-gradient-to-b from-white to-white/70 dark:shadow-slate-400 bg-primary dark:bg-slate-300 rounded-md shadow-lg active:shadow-sm focus:shadow-sm transition duration-200 ease-in-out"
-            />
-
-            <div className="bg-theme-cart rounded text-slate-600 font-medium lg:text-xs w-7 h-6 lg:h-5 lg:w-6 flex items-center justify-center">
-              {cartQuantity}
-            </div>
-            <Button
-              type="button"
-              svgClass="btn-cart dark:fill-slate-800"
-              svgURL="#icon-add"
-              ariaLabel="add product quantity"
-              onClick={handleIncrementItem}
-              backgroundColor="bg-transparent"
-              className="h-10 px-6 text-sm  flex justify-center items-center bg-gradient-to-b from-white to-white/70 dark:shadow-slate-400 bg-primary dark:bg-slate-300 rounded-md shadow-lg active:shadow-sm focus:shadow-sm transition duration-200 ease-in-out"
-            />
-          </div>
+          <Counter item={item} />
         </div>
       </div>
       <div className="grid items-center gap-5">
