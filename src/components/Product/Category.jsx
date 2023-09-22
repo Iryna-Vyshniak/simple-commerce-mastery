133202b7ed637094886e8e53f4d0ab0fb134ec4b
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-23 01:44:28 +0300

    Added the ability to display product colors as selectable dots, added UI styling

--- src/components/Product/Category.jsx
+++ src/components/Product/Category.jsx
@@ -1,8 +1,17 @@
-const Category = ({ category }) => {
+const Category = ({ category, className, classWrapper }) => {
   return (
-    <ul className="flex items-center justify-center gap-4 w-full">
+    <ul
+      className={
+        classWrapper
+          ? `${classWrapper} flex gap-4`
+          : 'flex items-center justify-center gap-4 w-full'
+      }
+    >
       {category.map((item, idx) => (
-        <li key={idx} className="w-max py-1 px-4 shadow-lg rounded-md">
+        <li
+          key={idx}
+          className={className ? `${className}` : 'min-w-max py-1 px-4 shadow-lg rounded-md'}
+        >
           <p className="text-sm font-semibold dark:text-slate-400 dark:drop-shadow-[0_0.03px_0.5px_rgba(0,0,20,1)]">
             {item}
           </p>
--- src/components/Product/ColorsBlock.jsx
+++ src/components/Product/ColorsBlock.jsx
@@ -1,20 +1,53 @@
-const ColorsBlock = ({ colors }) => {
+import { uniqueValues } from '../../shared/utils';
+
+const ColorsBlock = ({ colors, products, activeColors }) => {
   return (
     <ul className="flex colors-center justify-center gap-2">
-      {colors.map((color, idx) => (
-        <li
-          key={idx}
-          className={`h-5 w-5 rounded-full ${
-            color
-              ? color === 'white'
-                ? `bg-${color}`
-                : color === 'slate'
-                ? `bg-${color}-900`
-                : `bg-${color}-500`
-              : ''
-          } border border-slate-400 shadow-lg`}
-        ></li>
-      ))}
+      {colors && (
+        <>
+          {colors.map((color, idx) => (
+            <li
+              key={idx}
+              className={`h-5 w-5 rounded-full ${
+                color
+                  ? color === 'white'
+                    ? `bg-${color}`
+                    : color === 'slate'
+                    ? `bg-${color}-900`
+                    : `bg-${color}-500`
+                  : ''
+              } border border-slate-400 shadow-lg`}
+            ></li>
+          ))}
+        </>
+      )}
+      {products && (
+        <>
+          {uniqueValues(products, 'color').map((color, idx) => {
+            const isActiveColor = activeColors.includes(color);
+            const isWhite = color === 'white';
+            const isSlate = color === 'slate';
+            return (
+              <li
+                key={idx}
+                className={`h-5 w-5 rounded-full ${
+                  color
+                    ? isWhite
+                      ? `bg-${color}`
+                      : isSlate
+                      ? `bg-${color}-900`
+                      : `bg-${color}-500`
+                    : ''
+                }  ${
+                  isActiveColor
+                    ? 'ring-2 ring-slate-200 shadow-lg shadow-slate-400'
+                    : 'shadow-inner shadow-white/50 border border-slate-500'
+                }`}
+              ></li>
+            );
+          })}
+        </>
+      )}
     </ul>
   );
 };
