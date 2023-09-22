5ed9e13e51d2ec51fb24cf7902d9dda791b83378
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-09-22 03:41:59 +0300

    Add ShoeCard component for dynamic displaying shoe images

--- src/components/Product/ShoeCard.jsx
+++ src/components/Product/ShoeCard.jsx
@@ -1,28 +1,49 @@
 import Image from '../Image/Image';
 
-const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+const ShoeCard = ({
+  imgURL,
+  changeBigShoeImage,
+  bigShoeImg,
+  width,
+  height,
+  className,
+  divClassName
+}) => {
   const handleClick = () => {
-    if (bigShoeImg !== imgURL.bigShoe) {
-      changeBigShoeImage(imgURL.bigShoe);
+    if (bigShoeImg !== imgURL?.bigShoe) {
+      changeBigShoeImage(imgURL?.bigShoe);
     }
   };
 
   return (
     <li
       className={`border-2 rounded-xl ${
-        bigShoeImg === imgURL.bigShoe ? 'border-deep-red shadow-lg' : 'border-white'
+        bigShoeImg === imgURL?.bigShoe ? 'border-deep-red shadow-lg' : 'border-white'
       } cursor-pointer max-sm:flex-1 shadow-xl transition duration-200 ease-in-out`}
       onClick={handleClick}
     >
-      <div className="flex justify-center items-center w-[128px] h-[104px] bg-primary bg-gradient-to-b from-white to-white/5  dark:shadow-slate-400 dark:bg-slate-500 sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+      {divClassName ? (
+        <div className="flex justify-center items-center w-[128px] h-[104px] bg-primary bg-gradient-to-b from-white to-white/5  dark:shadow-slate-400 dark:bg-slate-500 sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+          <Image
+            src={imgURL?.thumbnail}
+            alt="shoe collection"
+            width={width}
+            height={height}
+            className={className ? `${className}` : 'object-cover object-center'}
+          />
+        </div>
+      ) : (
         <Image
-          src={imgURL.thumbnail}
+          src={imgURL?.thumbnail}
           alt="shoe collection"
-          width={127}
-          height={104}
-          className="object-cover object-center"
+          width={width}
+          height={height}
+          className={
+            className +
+            'bg-primary bg-gradient-to-b from-white to-white/5  dark:shadow-slate-400 dark:bg-slate-500 sm:w-40 sm:h-40 rounded-xl max-sm:p-4'
+          }
         />
-      </div>
+      )}
     </li>
   );
 };
