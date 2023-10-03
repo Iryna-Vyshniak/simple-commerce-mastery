625e5e8c7f722bb71cb39f251421458b1e775406
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-10-03 17:48:18 +0300

    added toastify options

--- src/shared/utils/index.js
+++ src/shared/utils/index.js
@@ -1,4 +1,7 @@
 export const uniqueValues = (values, property) => {
+  if (!values) return null;
+  if (!values.length) return null;
+
   const allValues = values.flatMap(product => product[property]);
   const unique = new Set(allValues);
   const uniqueValues = [...unique].sort((a, b) => a - b);
@@ -10,4 +13,18 @@ export const getRandomValue = value => {
 
   const randomIdx = Math.floor(Math.random() * value.length);
   return value[randomIdx];
+};
+
+const customId = 'custom-id-yes';
+
+export const toastifyOptions = {
+  position: 'top-left',
+  autoClose: 2500,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: 'colored',
+  toastId: customId
 };
